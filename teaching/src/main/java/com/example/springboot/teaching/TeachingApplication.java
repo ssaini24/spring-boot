7f30dd674891a23325386ceb92c41a2902d22365
vegita24 <sahil242saini@gmail.com>
2024-06-13 18:11:55 +0530

    stop context....

--- teaching/src/main/java/com/example/springboot/teaching/TeachingApplication.java
+++ teaching/src/main/java/com/example/springboot/teaching/TeachingApplication.java
@@ -2,11 +2,15 @@ package com.example.springboot.teaching;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "com.example.springboot.teaching")
 @SpringBootApplication
 public class TeachingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TeachingApplication.class, args);
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(TeachingApplication.class, args);
+//		applicationContext.stop();
 	}
 }
