03b0ded36ef2dcb4b9b619efc4717c96505022d4
vegita24 <sahil242saini@gmail.com>
2025-02-11 22:08:44 +0530

    boilerplate...

---
+++ custom-annotations/src/main/java/com/project/custom_annotations/CustomAnnotationsApplication.java
@@ -0,0 +1,13 @@
+package com.project.custom_annotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomAnnotationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomAnnotationsApplication.class, args);
+	}
+
+}
---
+++ custom-annotations/src/test/java/com/project/custom_annotations/CustomAnnotationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.custom_annotations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomAnnotationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
